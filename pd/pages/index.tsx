a05ce5cba034620ee7b43548e21b038c7036a808
Tiamat-KIT <100649020+Tiamat-KIT@users.noreply.github.com>
2022-09-30 15:15:32 +0900

    ルートの種類表追加
    ルートの種類表をオブジェクトのキーのmap関数を利用して実装。

--- pd/pages/index.tsx
+++ pd/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+//import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar,Nav,Col,Row,Card } from "react-bootstrap";
 import Link from 'next/link';
+import {Property} from 'csstype';
+
+type route_obj = {
+  color:NonNullable<Property.Color>
+  route_name:NonNullable<string>,
+  route_startpoint:NonNullable<string>,
+  route_endpoint:NonNullable<string>
+}
+
+const route_variation : route_obj[] = [
+  {color:"red",route_name:"中央",route_startpoint:"フォルテ",route_endpoint:"若松"},
+  {color:"blue",route_name:"西部",route_startpoint:"野々市市役所",route_endpoint:"藤平田南"},
+  {color:"green",route_name:"北部",route_startpoint:"フォルテ",route_endpoint:"本町6丁目"},
+  {color:"yellow",route_name:"南部",route_startpoint:"野々市市役所",route_endpoint:"矢作西"}
+]
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
 
       <Navbar bg="warning" expand="md" color='yellow-400'>
         <Navbar.Toggle className='me-auto'/>
@@ -29,14 +43,17 @@ export default function Home() {
       <div style={{padding : "15px"}}/>
 
       <Row xs={2} md={2} className="g-2">
-        {Array.from({ length: 4 }).map((_, idx) => (
+        {Object.keys(route_variation).map((_, idx) => (
         <Col>
           <Card>
             {/*<Card.Img variant="top" src="https://thumb.ac-illust.com/6c/6c49457122b22005a8dcacaac54d2c17_t.jpeg" />*/}
             <Card.Body >
-            <Card.Title>Route</Card.Title>
+            <Card.Title>{route_variation[idx].route_name+"ルート"}</Card.Title>
               <Card.Text>
-                ○○~○○ 
+                {route_variation[idx].route_startpoint}~{route_variation[idx].route_endpoint} 
+              </Card.Text>
+              <Card.Text>
+                このルートのバス停一覧を見る
               </Card.Text>
             </Card.Body>
           </Card>
