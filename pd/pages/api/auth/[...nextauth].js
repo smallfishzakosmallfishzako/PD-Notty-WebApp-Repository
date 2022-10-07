838c570b55b03e292b561e86b778bbbd2d80f7a4
Tiamat-KIT <100649020+Tiamat-KIT@users.noreply.github.com>
2022-10-07 15:34:58 +0900

    うごいたわ
    わからん

--- pd/pages/api/auth/[...nextauth].js
+++ pd/pages/api/auth/[...nextauth].js
@@ -1,25 +1,25 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    callbacks:{
-        async jwt({token,account}){
-            if(account){
-                token.accesToken = account.access_token;
-            }
-            return token;
-        },
-        async session({session,token,user}){
-            session.accesToken = token.accesToken;
-            return session;
-        }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  callbacks: {
+    //jwtが作成・更新された時に呼ばれる
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
-    secret: SECRET,
-    debug:true
+    //セッションがチェックされた時に呼ばれる
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
